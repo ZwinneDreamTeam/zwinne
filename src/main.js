46e25eb30fdf6f85b1d6167e7821bb1ab9b60769
Bartłomiej Stoliński <b.stolinski@fivedottwelve.com>
2018-03-19 14:00:12 +0100

    connect to firebase

--- src/main.js
+++ src/main.js
@@ -3,8 +3,19 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import firebase from 'firebase'
 
 Vue.config.productionTip = false
+
+var config = {
+  apiKey: "AIzaSyAduGsOpgqCLn79cI4fzBvMsC0LFfnQhWA",
+  authDomain: "zwinnexd.firebaseapp.com",
+  databaseURL: "https://zwinnexd.firebaseio.com",
+  projectId: "zwinnexd",
+  storageBucket: "zwinnexd.appspot.com",
+  messagingSenderId: "427115139234"
+};
+firebase.initializeApp(config);
 
 /* eslint-disable no-new */
 new Vue({
