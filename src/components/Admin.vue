1d782f2f628a3c5a9a16b1acec4424ec0e1920e5
Bartłomiej Stoliński <b.stolinski@fivedottwelve.com>
2018-03-20 22:41:07 +0100

    add admin, candidate, moderator pages

---
+++ src/components/Admin.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>{{msg}}</div>
+</template>
+
+<script>
+  export default {
+    name: 'Admin',
+    data() {
+      return {
+        msg: 'Admin'
+      }
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Admin from '@/components/Admin'
+import Candidate from '@/components/Candidate'
+import Moderator from '@/components/Moderator'
 import Home from '@/components/Home'
 import Login from '@/components/Login'
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 import firebase from 'firebase'
+import toastr from 'toastr'
 
 Vue.use(VueMaterial);
 Vue.use(VueRouter);
@@ -27,6 +31,33 @@ let router = new VueRouter({
       meta: {
         requiresAuth: true
       }
+    },
+    {
+      path: '/admin',
+      name: 'Admin',
+      component: Admin,
+      meta: {
+        requiresAuth: true,
+        requiresAdmin: true
+      }
+    },
+    {
+      path: '/candidate',
+      name: 'Candidate',
+      component: Candidate,
+      meta: {
+        requiresAuth: true,
+        requiresCandidate: true
+      }
+    },
+    {
+      path: '/moderator',
+      name: 'Moderator',
+      component: Moderator,
+      meta: {
+        requiresAuth: true,
+        requiresModerator: true
+      }
     }
   ]
 });
@@ -34,6 +65,13 @@ let router = new VueRouter({
 
 router.beforeEach((to, from, next) => {
   let currentUser = firebase.auth().currentUser;
+
+  if (currentUser) {
+    let db = firebase.database();
+    let currentUserDb = db.ref('users').child(currentUser.uid);
+    toastr.success(currentUserDb.child('isCandidate').valueOf().toString());
+  }
+
   let requiresAuth = to.matched.some(record => record.meta.requiresAuth);
 
   if (requiresAuth && !currentUser) next('login')
