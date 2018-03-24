5a4408a0bf155e70de720dfa36ebbc506530c70b
Iza <195749@edu.p.lodz.pl>
2018-03-24 21:34:18 +0100

    User details view, without styles.

--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -1,8 +1,22 @@
 <template>
   <div>
     <h1 >User details </h1>
-    <md-input> {{getUser().username }} </md-input>
-    <div> {{getUser().email }} </div>
+
+    <div>
+      Username:
+      <input v-model="username" :placeholder="getUser().username" disabled>
+    </div>
+    <div>
+      Email:
+      <input v-model="email" :placeholder="getUser().email" disabled>
+    </div>
+    <div>
+      Permissions:
+      <div><md-switch v-model="boolean" :value="getUser().isModerator" disabled> Moderator </md-switch></div>
+      <div><md-switch v-model="boolean" :value="getUser().isCandidate" disabled> Candidate </md-switch></div>
+      <div><md-switch v-model="boolean" :value="getUser().isRedactor" disabled> Redactor </md-switch></div>
+    </div>
+    <md-button v-on:click="editUser" class="md-raised"> {{edit}}</md-button>
   </div>
 </template>
 
@@ -21,10 +35,27 @@ export default {
                     console.log(user.isModerator);
            });
          return user;
+      },
+      editUser: function (event) {
+        //TODO
       }
-    }
+    },
+    data: () => ({
+        email: "",
+        username: "",
+        isModerator: "",
+        isCandidate: "",
+        boolean: false,
+        edit: "Edit"
+    }),
   }
 </script>
 
 <style scoped>
+.md-switch {
+    display: flex;
+  }
+.md-button {
+
+}
 </style>
