fe96bc33c27c49b91b56eefbf374279ad4ffcf37
adfry <adfry@stud.ics.p.lodz.pl>
2018-03-25 14:54:44 +0200

    Setting empty strings as values enabled.

--- src/components/UserDetails.vue
+++ src/components/UserDetails.vue
@@ -3,17 +3,17 @@
     <h1>User details </h1>
     <div>
       Username:
-      <input v-model="username" :placeholder="getUser().username" :disabled="disabled == 1">
+      <input v-model="username" :disabled="disabled == 1">
     </div>
     <div>
       Email:
-      <input v-model="email" :placeholder="getUser().email" :disabled="disabled == 1">
+      <input v-model="email" :disabled="disabled == 1">
     </div>
     <div>
       Permissions:
-      <div><md-switch v-model="isModerator" :disabled="disabled == 1"> Moderator {{isModerator}}</md-switch></div>
-      <div><md-switch v-model="isCandidate" :disabled="disabled == 1"> Candidate {{isCandidate}}</md-switch></div>
-      <div><md-switch v-model="isRedactor" :disabled="disabled == 1"> Redactor {{isRedactor}}</md-switch></div>
+      <div><md-switch v-model="isModerator" :disabled="disabled == 1"> Moderator</md-switch></div>
+      <div><md-switch v-model="isCandidate" :disabled="disabled == 1"> Candidate</md-switch></div>
+      <div><md-switch v-model="isRedactor" :disabled="disabled == 1"> Redactor</md-switch></div>
     </div>
     <md-button @click="disabled = 0" class="md-raised" v-show="disabled == 1"> {{edit}}</md-button>
     <md-button @click="disabled = 1" v-on:click="applyChanges" class="md-raised" v-show="disabled == 0"> {{apply}}</md-button>
@@ -22,8 +22,6 @@
 
 <script>
 import { db } from "../App"
-
-let initialized = false;
 
 export default {
     name: "user-details",
@@ -36,33 +34,33 @@ export default {
            user = snapshot.val();
          });
 
-        if(!initialized){
-          this.$data.isModerator = user.isModerator;
-          this.$data.isCandidate = user.isCandidate;
-          this.$data.isRedactor = user.isRedactor;
-          initialized = true;
-        }
-
-         return user;
+        this.$data.username = user.username;
+        this.$data.email = user.email;
+        this.$data.isModerator = user.isModerator;
+        this.$data.isCandidate = user.isCandidate;
+        this.$data.isRedactor = user.isRedactor;
       },
       applyChanges: function() {
         var userData = {
-          email: (this.$data.email == "") ? this.getUser().email : this.$data.email,
+          email: this.$data.email,
           isCandidate: this.$data.isCandidate,
           isModerator: this.$data.isModerator,
           isRedactor: this.$data.isRedactor,
-          username: (this.$data.username == "") ? this.getUser().username : this.$data.username
+          username: this.$data.username
         };
 
         return db.ref('/users/' + this.$route.params.id).set(userData);
       }
     },
+    beforeMount(){
+      this.getUser();
+    },
     data: () => ({
         email: "",
         username: "",
-        isModerator: true,
-        isCandidate: true,
-        isRedactor: true,
+        isModerator: "",
+        isCandidate: "",
+        isRedactor: "",
         edit: "Edit",
         apply: "Apply",
         disabled: 1
