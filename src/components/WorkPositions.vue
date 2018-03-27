f704373f1f0b26a7526a65faead310b7c7817e88
Patryk Kacperski <221599@edu.p.lodz.pl>
2018-03-27 17:45:04 +0200

    Added list of work positions with filtering button

--- src/components/WorkPositions.vue
+++ src/components/WorkPositions.vue
@@ -1,11 +1,42 @@
 <template>
-
+  <div>
+    <h1>Available positions</h1>
+    <input type="checkbox" id="positionsCheckbox" v-model="positionFilter" true-value="t" false-value="f"/>
+    <label for="positionsCheckbox">Active positions only</label>
+    <ul id="positions-list">
+      <li v-for="(position, key) of filteredPositions">
+        {{ position.name }} See details
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
-    export default {
-        name: "work-positions"
+  import { db } from "../App"
+
+  let positionsRef = db.ref('workPositions');
+
+  export default {
+    name: "work-positions",
+    firebase: {
+      positions: positionsRef
+    },
+    data () {
+      return {
+        positionFilter: ""
+      }
+    },
+    computed: {
+      filteredPositions: function () {
+        if (this.positionFilter === 't') {
+          return this.positions.filter((position) => {
+            return position.isActive;
+          })
+        }
+        return this.positions
+      }
     }
+  }
 </script>
 
 <style scoped>
