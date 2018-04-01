ec01cf4c7665cd33e5a796f4d865a920183b5eb5
Marcin Szalek <marcin.szalek1@gmail.com>
2018-04-01 14:31:11 +0200

    Refactored User list

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,21 +1,26 @@
 <template>
   <div>
-    <input type="checkbox" id="redactorsCheckbox" v-model="redactorFilter">
-    <label for="redactorsCheckbox">Redactors only</label>
-
-    <md-table v-model="filteredUsers" md-card md-sort="username" md-sort-order="asc">
+    <md-table v-model="users" :md-sort.sync="currentSort" :md-sort-order.sync="currentSortOrder"
+              :md-sort-fn="customSort" md-card>
       <md-table-toolbar>
         <h1 class="md-title">Użytkownicy</h1>
       </md-table-toolbar>
 
-      <md-table-row slot="md-table-row" slot-scope="{item}">
+      <md-table-row slot="md-table-row" slot-scope="{item}" md-selectable="single" @click.native="onSelect(item)">
         <md-table-cell md-label="Nazwa" md-sort-by="username">{{ item.username }}</md-table-cell>
         <md-table-cell md-label="Email" md-sort-by="email">{{ item.email }}</md-table-cell>
-        <md-table-cell md-label="Kandydat" md-sort-by="candidate">{{ item.isCandidate }}</md-table-cell>
-        <md-table-cell md-label="Redaktor" md-sort-by="redactor">{{ item.isRedactor}}</md-table-cell>
-        <md-table-cell md-label="Moderator" md-sort-by="moderator">{{ item.isModerator }}</md-table-cell>
-        <!--<md-table-cell><router-link :to="{ name: 'userDetails', params: { id: user['.key'] }}">See details</router-link></md-table-cell>-->
-
+        <md-table-cell md-label="Kandydat" md-sort-by="isCandidate">
+          <md-icon class="iconCheck" v-if="item.isCandidate">check</md-icon>
+          <md-icon class="iconClose" v-if="!item.isCandidate">close</md-icon>
+        </md-table-cell>
+        <md-table-cell md-label="Redaktor" md-sort-by="isRedactor">
+          <md-icon class="iconCheck" v-if="item.isRedactor">check</md-icon>
+          <md-icon class="iconClose" v-if="!item.isRedactor">close</md-icon>
+        </md-table-cell>
+        <md-table-cell md-label="Moderator" md-sort-by="isModerator">
+          <md-icon class="iconCheck" v-if="item.isModerator">check</md-icon>
+          <md-icon class="iconClose" v-if="!item.isModerator">close</md-icon>
+        </md-table-cell>
       </md-table-row>
     </md-table>
   </div>
@@ -34,21 +39,52 @@
     data() {
       return {
         redactorFilter: false,
+        currentSort: 'username',
+        currentSortOrder: 'asc',
       }
     },
-    computed: {
-      filteredUsers: function () {
-        if (this.redactorFilter) {
-          return this.users.filter((user) => {
-            return user.isRedactor;
-          })
+    methods: {
+      compareBooleans(a, b, sortBy) {
+        if (a[sortBy] === b[sortBy])
+          return 0;
+        else if (a[sortBy] && !b[sortBy]) {
+          return this.currentSortOrder === 'desc' ? 1 : -1;
+        } else {
+          return this.currentSortOrder === 'desc' ? -1 : 1;
         }
-        console.log(this.users);
-        return this.users
-      }
-    }
+
+      },
+      compareStrings(a, b, sortBy) {
+        if (this.currentSortOrder === 'desc') {
+          return a[sortBy].localeCompare(b[sortBy])
+        } else {
+          return b[sortBy].localeCompare(a[sortBy])
+        }
+      },
+      customSort(value) {
+        return value.sort((a, b) => {
+          const sortBy = this.currentSort;
+
+          if (typeof(a[sortBy]) === "boolean") {
+            return this.compareBooleans(a, b, sortBy);
+          } else {
+            return this.compareStrings(a, b, sortBy);
+          }
+        })
+      },
+      onSelect(item) {
+        this.$router.push({name: 'userDetails', params: {id: item['.key']}});
+      },
+    },
   }
 </script>
 
 <style scoped>
+  .iconCheck {
+    color: green !important;
+  }
+
+  .iconClose {
+    color: darkred !important;
+  }
 </style>
