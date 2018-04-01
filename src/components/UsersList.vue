f5062d94e54eb250786c94c8eaa64af63be27002
Marcin Szalek <marcin.szalek1@gmail.com>
2018-04-01 10:18:33 +0200

    Initial user list refactor

--- src/components/UsersList.vue
+++ src/components/UsersList.vue
@@ -1,38 +1,49 @@
 <template>
   <div>
-    <h1>UZERS :DDD</h1>
-    <input type="checkbox" id="redactorsCheckbox" v-model="redactorFilter" true-value="t" false-value="f">
+    <input type="checkbox" id="redactorsCheckbox" v-model="redactorFilter">
     <label for="redactorsCheckbox">Redactors only</label>
-    <ul id="user-list">
-      <li v-for="(user, key) of filteredUsers">
-        {{ user.username }} <router-link :to="{ name: 'userDetails', params: { id: user['.key'] }}">See details </router-link>
-      </li>
-    </ul>
+
+    <md-table v-model="filteredUsers" md-card md-sort="username" md-sort-order="asc">
+      <md-table-toolbar>
+        <h1 class="md-title">Użytkownicy</h1>
+      </md-table-toolbar>
+
+      <md-table-row slot="md-table-row" slot-scope="{item}">
+        <md-table-cell md-label="Nazwa" md-sort-by="username">{{ item.username }}</md-table-cell>
+        <md-table-cell md-label="Email" md-sort-by="email">{{ item.email }}</md-table-cell>
+        <md-table-cell md-label="Kandydat" md-sort-by="candidate">{{ item.isCandidate }}</md-table-cell>
+        <md-table-cell md-label="Redaktor" md-sort-by="redactor">{{ item.isRedactor}}</md-table-cell>
+        <md-table-cell md-label="Moderator" md-sort-by="moderator">{{ item.isModerator }}</md-table-cell>
+        <!--<md-table-cell><router-link :to="{ name: 'userDetails', params: { id: user['.key'] }}">See details</router-link></md-table-cell>-->
+
+      </md-table-row>
+    </md-table>
   </div>
 </template>
 
 <script>
-import { db } from "../App"
+  import firebase from 'firebase';
 
-  let usersRef = db.ref('users');
+  let db = firebase.database();
 
-export default {
+  export default {
     name: "users-list",
     firebase: {
-      users: usersRef
+      users: db.ref('users')
     },
-    data () {
+    data() {
       return {
-        redactorFilter: ""
+        redactorFilter: false,
       }
     },
     computed: {
       filteredUsers: function () {
-        if (this.redactorFilter === 't') {
+        if (this.redactorFilter) {
           return this.users.filter((user) => {
             return user.isRedactor;
           })
         }
+        console.log(this.users);
         return this.users
       }
     }
