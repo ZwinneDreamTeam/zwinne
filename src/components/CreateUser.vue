235409cab24353b0641b500d798aab501fdc42dc
Jarosław Roliński <221652@edu.p.lodz.pl>
2018-04-01 21:16:22 +0200

    User fields validation.

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -5,25 +5,25 @@
         <h1 class="md-title">Create new account</h1>
       </md-card-header>
 
-      <md-field>
+      <md-field :class="usernameClass">
         <md-icon>person</md-icon>
         <label>Username</label>
-        <md-input v-model="username" type="text" required/>
-        <span class="md-error">There is an error</span>
+        <md-input v-model="username" type="text" v-on:keyup="fromIncorrecUsernameToCorrect()" v-on:blur="isUsernameValid()" required/>
+        <span class="md-error">A username must contain at least five characters</span>
       </md-field>
 
-      <md-field>
+      <md-field :class="emailClass">
         <md-icon>email</md-icon>
         <label>Email</label>
-        <md-input v-model="email" type="email" required/>
-        <span class="md-error">There is an error</span>
+        <md-input v-model="email" type="email" v-on:keyup="fromIncorrecEmailToCorrect()" v-on:blur="isEmailValid()" required/>
+        <span class="md-error">Please provide valid email address</span>
       </md-field>
 
-      <md-field>
+      <md-field :class="passwordClass">
         <md-icon>lock</md-icon>
         <label>Password</label>
-        <md-input v-model="password" type="password" required/>
-        <span class="md-error">There is an error</span>
+        <md-input v-model="password" type="password" v-on:keyup="fromIncorrecPasswordToCorrect" v-on:blur="isPasswordValid()" required/>
+        <span class="md-error">A password must contain at least eight characters</span>
       </md-field>
 
       <div>
@@ -87,16 +87,69 @@
       edit: "Edit",
       apply: "Apply",
       password: "",
-      addUser: "Submit"
+      addUser: "Submit",
+      validEmail: true,
+      validUsername: true,
+      validPassword: true
     }),
     methods: {
       submit_click: function (event) {
+        if (this.isValid()) {
+          createAccountFirebase.createUserWithEmailAndPassword(this.$data.email, this.$data.password)
+            .then(user => createUser(user, this.$data.isCandidate, this.$data.isModerator, this.$data.isRedactor, this.$data.username))
+            .catch(function (error) {
+              alert(error.message);
+            });
+        }
+      },
+      isValid: function () {
 
-        createAccountFirebase.createUserWithEmailAndPassword(this.$data.email, this.$data.password)
-          .then(user => createUser(user, this.$data.isCandidate, this.$data.isModerator, this.$data.isRedactor, this.$data.username))
-          .catch(function (error) {
-            alert(error.message);
-          });
+        let passwordValid = this.isPasswordValid();
+        let emailValid = this.isEmailValid();
+        let usernameValid = this.isUsernameValid();
+        return emailValid && passwordValid && usernameValid;
+      },
+      isPasswordValid: function () {
+        this.$data.validPassword = this.$data.password.length >= 5;
+        return this.$data.validPassword;
+      },
+      isUsernameValid: function () {
+        this.$data.validUsername = this.$data.username.length >= 5;
+        return this.$data.validUsername;
+      },
+      isEmailValid: function () {
+        let re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        this.$data.validEmail = re.test(this.$data.email);
+        return this.$data.validEmail;
+      },
+      fromIncorrecEmailToCorrect: function(){
+        if(this.$data.validEmail) return;
+        this.isEmailValid();
+      },
+      fromIncorrecPasswordToCorrect: function(){
+        if(this.$data.validPassword) return;
+        this.isPasswordValid();
+      },
+      fromIncorrecUsernameToCorrect: function(){
+        if(this.$data.validUsername) return;
+        this.isUsernameValid();
+      }
+    },
+    computed: {
+      usernameClass() {
+        return {
+          'md-invalid': !this.validUsername
+        }
+      },
+      emailClass() {
+        return {
+          'md-invalid': !this.validEmail
+        }
+      },
+      passwordClass() {
+        return {
+          'md-invalid': !this.validPassword
+        }
       }
     }
   }
