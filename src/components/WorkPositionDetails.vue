3fdadfef1c12cc7807e0ef825723735e625a75ec
adfry <adfry@stud.ics.p.lodz.pl>
2018-04-06 16:25:10 +0200

    Validation added to work position editing. Labels in polish.

--- src/components/WorkPositionDetails.vue
+++ src/components/WorkPositionDetails.vue
@@ -1,33 +1,42 @@
 <template>
+<div>
   <md-card class="positionDetails">
-    <h1>Position details</h1>
-    <div>
-      <md-field>
-        <label>Name</label>
-        <md-input v-model="position.name" type="text" :disabled="disabled"/>
+      <md-card-header>
+        <h1 class="md-title">Szczegóły stanowiska</h1>
+      </md-card-header>
+
+      <md-field :class="nameClass">
+        <label>Nazwa stanowiska</label>
+        <md-input v-model="position.name" type="text" :disabled="disabled"
+              v-on:keyup="validateNameIfIncorrect()" v-on:blur="validateName()" required/>
+        <span class="md-error">Nazwa stanowiska musi zawierać co najmniej 2 znaki</span>
       </md-field>
-    </div>
-    <div>
-      <md-field>
-        <label>Company</label>
-        <md-input v-model="position.company" type="text" :disabled="disabled"/>
+
+      <md-field :class="companyClass">
+        <label>Nazwa firmy</label>
+        <md-input v-model="position.company" type="text" :disabled="disabled"
+            v-on:keyup="validateCompanyIfIncorrect()" v-on:blur="validateCompany()" required/>
+        <span class="md-error">Nazwa firmy musi zawierać co najmniej 2 znaki</span>
       </md-field>
-    </div>
-    <div>
-      <md-field>
-        <label>Description</label>
-        <md-textarea v-model="position.description" type="text" :disabled="disabled"/>
+
+      <md-field :class="descriptionClass">
+        <label>Opis stanowiska</label>
+        <md-textarea v-model="position.description" type="text" :disabled="disabled"
+            v-on:keyup="validateDescriptionIfIncorrect()" v-on:blur="validateDescription()" required/>
+        <span class="md-error">Opis stanowiska musi zawierać co najmniej 2 znaki</span>
       </md-field>
-    </div>
+
     <div>
-      <md-switch class="md-primary" v-model="position.isActive" :disabled="disabled">Active</md-switch>
+      <md-switch class="md-primary" v-model="position.isActive" :disabled="disabled">Aktywne</md-switch>
     </div>
+
     <md-button class="md-primary md-raised" @click="disabled = false" v-if="disabled && canEdit">Edytuj</md-button>
     <md-button class="md-primary md-raised" @click="disabled = true" v-on:click="applyChanges"
                v-if="!disabled && canEdit">
-      Zatwierdz
+      Zapisz
     </md-button>
   </md-card>
+  </div>
 </template>
 
 <script>
@@ -49,12 +58,59 @@
     },
     methods: {
       applyChanges() {
-        return db.ref('/workPositions/' + this.$route.params.id).set(this.position);
+      if(this.isValid()) {
+          return db.ref('/workPositions/' + this.$route.params.id).set(this.position);
+        }
+      },
+      isValid: function () {
+        let nameValid = this.validateName();
+        let companyValid = this.validateCompany();
+        let descriptionValid = this.validateDescription();
+        return nameValid && companyValid && descriptionValid;
+      },
+      validateName: function () {
+        this.$data.validName = this.position.name.length >= 2;
+        return this.$data.validName;
+      },
+      validateNameIfIncorrect: function () {
+        if (this.$data.validName) return;
+        this.validateName();
+      },
+      validateCompany: function () {
+        this.$data.validCompany = this.position.company.length >= 2;
+        return this.$data.validCompany;
+      },
+      validateCompanyIfIncorrect: function () {
+        if (this.$data.validCompany) return;
+        this.validateCompany();
+      },
+      validateDescription: function () {
+        this.$data.validDescription = this.position.description.length >= 2;
+        return this.$data.validDescription;
+      },
+      validateDescriptionIfIncorrect: function () {
+        if (this.$data.validDescription) return;
+        this.validateDescription();
       }
     },
     computed: {
       canEdit() {
         return this.isModerator || this.isRedactor;
+      },
+      nameClass() {
+        return {
+          'md-invalid': !this.validName
+        }
+      },
+      companyClass() {
+        return {
+          'md-invalid': !this.validCompany
+        }
+      },
+      descriptionClass() {
+        return {
+          'md-invalid': !this.validDescription
+        }
       }
     },
 
@@ -63,6 +119,9 @@
       disabled: true,
       isModerator: false,
       isRedactor: false,
+      validName: true,
+      validCompany: true,
+      validDescription: true
     }),
   }
 </script>
