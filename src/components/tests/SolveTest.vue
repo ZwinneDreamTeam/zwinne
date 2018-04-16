aa45625ad5b59c401233ffa5ab2735084430f963
Patryk Kacperski <221599@edu.p.lodz.pl>
2018-04-16 21:19:03 +0200

    Added selecting test language at the beginning

--- src/components/tests/SolveTest.vue
+++ src/components/tests/SolveTest.vue
@@ -1,26 +1,61 @@
 <template>
     <div>
-      <md-card>
+      <md-card class="solveTestCard">
         <md-card-header>
           <md-card-header-text>
-            <h3 class="md-title">Test</h3>
+            <h3 class="md-title">Test: {{testName}}</h3>
           </md-card-header-text>
         </md-card-header>
+
+        <md-field>
+          <label>Wybierz język</label>
+          <md-select class="mySelect" v-model="language" :disabled="isLanguageSelected">
+            <md-option value="pl">Polski</md-option>
+            <md-option value="en">Angielski</md-option>
+          </md-select>
+        </md-field>
       </md-card>
+
+      <div v-if="isLanguageSelected" v-for="question in questions">
+        {{question.type}}
+      </div>
     </div>
 </template>
 
 <script>
-    export default {
-      name: "solve-test",
-      data() {
-        return {
+  import firebase from 'firebase';
 
-        };
+  export default {
+    name: "solve-test",
+    data() {
+      return {
+        language: "",
+        languages: [],
+        questions: [],
+        testName: "",
+        result: {},
+
+      };
+    },
+    mounted() {
+      let testKey = this.$route.params.id;
+      firebase.database().ref("tests/" + testKey).on('value', (snapshot) => {
+        let test = snapshot.val();
+        this.questions = test.questions;
+        this.testName = test.name;
+      })
+    },
+    computed: {
+      isLanguageSelected() {
+        return !(this.language.length === 0)
       }
     }
+  }
 </script>
 
 <style scoped>
-
+  .solveTestCard {
+    margin: 16px;
+    padding: 16px;
+  }
 </style>
