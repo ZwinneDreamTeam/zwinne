a7ab2848285b873b6e6f4f5ce4f79c4a7c29c1a1
Bartłomiej Stoliński <221661@edu.p.lodz.pl>
2018-04-16 11:40:05 +0200

    html to pdf

--- src/components/tests/questions/TestDetailQuestions.vue
+++ src/components/tests/questions/TestDetailQuestions.vue
@@ -2,19 +2,20 @@
   <div>
     <div class="md-table-head-label">Pytania</div>
 
-
-    <md-card v-for="question in questions" :key="question.name" class="questionCard">
-      <span class="questionName">{{question.name}}</span>
-      <div class="questionType">{{questionType(question.type)}}
-        <span v-if="question.type === 'scale'">({{question.scaleMin}} - {{question.scaleMax}})</span>
-        <span v-else-if="question.type === 'select'">
+    <section id="html-2-pdfwrapper">
+      <md-card v-for="question in questions" :key="question.name" class="questionCard">
+        <span class="questionName">{{question.name}}</span>
+        <div class="questionType">{{questionType(question.type)}}
+          <span v-if="question.type === 'scale'">({{question.scaleMin}} - {{question.scaleMax}})</span>
+          <span v-else-if="question.type === 'select'">
           <span v-for="(answer, index) in question.possibleAnswers">
             <span v-if="index === 0">: </span>
             <span v-else>, </span>
             {{answer}}</span>
         </span>
-      </div>
-    </md-card>
+        </div>
+      </md-card>
+    </section>
 
     <div>
       <md-button v-on:click="createPDF(questions)" class="md-raised md-primary downloadPdfButton">Pobierz PDF
@@ -35,7 +36,15 @@
     props: ['questions', 'mode'],
     emits: ['questionAdded'],
     data() {
-      return {};
+      return {
+        margins: {
+          top: 30,
+          bottom: 30,
+          left: 30,
+          width: 550
+        },
+
+      };
     },
     methods: {
       onQuestionSubmitted(question) {
@@ -54,45 +63,82 @@
         }
       },
       createPDF(questions) {
-        const doc = new jsPDF();
-        let height = 0;
-        for (let i = 0; i < this.questions.length; i++) {
-          doc.setFontSize(12);
-          height += 10;
+        var pdf = new jsPDF('p', 'pt', 'a4');
+        pdf.setFontSize(18);
+        pdf.fromHTML(document.getElementById('html-2-pdfwrapper'),
+          this.$data.margins.left, // x coord
+          this.$data.margins.top,
+          {
+            // y coord
+            width: this.$data.margins.width// max width of content on PDF
+          }, function (dispose) {
+            //headerFooterFormatting(pdf)
+          },
+          this.$data.margins);
 
-          doc.text((i + 1).toString() + ". " + questions[i].name, 10, height + 10);
+        pdf.save('test.pdf');
 
-          height += 6;
-          doc.setFontSize(10);
 
-          switch (questions[i].type) {
-            case 'text':
-              doc.setLineWidth(5);
-              doc.text('Pytanie otwarte', 10, height + 10);
-              break;
-            case 'scale':
-              doc.text('Pytanie skali (' + questions[i].scaleMin + ' - ' + questions[i].scaleMax + ')', 10, height + 10);
-              break;
-            case 'number':
-              doc.text('Pytanie liczbowe', 10, height + 10);
-              break;
-            case 'select':
-              let text = 'Pytanie wyboru: ';
-              for (let j = 0; j < this.questions[i].possibleAnswers.length; j++) {
-                if (j === 0) {
-                  text += questions[i].possibleAnswers[j];
-                } else {
-                  text += ", " + questions[i].possibleAnswers[j];
-                }
-              }
-              doc.text(text, 10, height + 10);
-              break;
-          }
+        // let pdf = new jsPDF('p', 'pt', 'letter'), source = this.$refs.page_content, specialElementHandlers = {
+        //   '#bypassme': function (element, renderer) {
+        //     return true
+        //   }
+        // };
+        // var margins = {
+        //   top: 60,
+        //   bottom: 60,
+        //   left: 40,
+        //   width: 522
+        // };
+        // pdf.fromHTML(source, margins.left, margins.top,
+        //   {
+        //     'width': margins.width
+        //     , 'elementHandlers': specialElementHandlers
+        //   },
+        //   function () {
+        //     pdf.save('download_pdf.pdf');
+        //   }, margins);
 
-          height += 5;
-        }
 
-        doc.save('test.pdf');
+        // const doc = new jsPDF();
+        // let height = 0;
+        // for (let i = 0; i < this.questions.length; i++) {
+        //   doc.setFontSize(12);
+        //   height += 10;
+        //
+        //   doc.text((i + 1).toString() + ". " + questions[i].name, 10, height + 10);
+        //
+        //   height += 6;
+        //   doc.setFontSize(10);
+        //
+        //   switch (questions[i].type) {
+        //     case 'text':
+        //       doc.setLineWidth(5);
+        //       doc.text('Pytanie otwarte', 10, height + 10);
+        //       break;
+        //     case 'scale':
+        //       doc.text('Pytanie skali (' + questions[i].scaleMin + ' - ' + questions[i].scaleMax + ')', 10, height + 10);
+        //       break;
+        //     case 'number':
+        //       doc.text('Pytanie liczbowe', 10, height + 10);
+        //       break;
+        //     case 'select':
+        //       let text = 'Pytanie wyboru: ';
+        //       for (let j = 0; j < this.questions[i].possibleAnswers.length; j++) {
+        //         if (j === 0) {
+        //           text += questions[i].possibleAnswers[j];
+        //         } else {
+        //           text += ", " + questions[i].possibleAnswers[j];
+        //         }
+        //       }
+        //       doc.text(text, 10, height + 10);
+        //       break;
+        //   }
+        //
+        //   height += 5;
+        // }
+        //
+        // doc.save('test.pdf');
       }
     },
   }
