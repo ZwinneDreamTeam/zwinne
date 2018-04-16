5a59e4e5a24860b0535bed38e1864fb131d16446
Bartłomiej Stoliński <221661@edu.p.lodz.pl>
2018-04-16 21:22:04 +0200

    add better formatted pdf

--- src/components/tests/questions/TestDetailQuestions.vue
+++ src/components/tests/questions/TestDetailQuestions.vue
@@ -2,6 +2,7 @@
   <div>
     <div class="md-table-head-label">Pytania</div>
 
+
     <section id="html-2-pdfwrapper">
       <md-card v-for="question in questions" :key="question.name" class="questionCard">
         <span class="questionName">{{question.name}}</span>
@@ -15,8 +16,8 @@
         </span>
         </div>
       </md-card>
-    </section>
 
+    </section>
     <div>
       <md-button v-on:click="createPDF(questions)" class="md-raised md-primary downloadPdfButton">Pobierz PDF
       </md-button>
@@ -28,7 +29,11 @@
 
 <script>
   import AddQuestion from "./AddQuestion";
-  import jsPDF from "jsPDF";
+  import pdfMake from "pdfmake/build/pdfmake";
+  import pdfFonts from "pdfmake/build/vfs_fonts";
+  import * as _ from "lodash";
+
+  pdfMake.vfs = pdfFonts.pdfMake.vfs;
 
   export default {
     components: {AddQuestion},
@@ -63,82 +68,74 @@
         }
       },
       createPDF(questions) {
-        var pdf = new jsPDF('p', 'pt', 'a4');
-        pdf.setFontSize(18);
-        pdf.fromHTML(document.getElementById('html-2-pdfwrapper'),
-          this.$data.margins.left, // x coord
-          this.$data.margins.top,
-          {
-            // y coord
-            width: this.$data.margins.width// max width of content on PDF
-          }, function (dispose) {
-            //headerFooterFormatting(pdf)
+        let objectsToReturn = [];
+
+        let processedQuestions = _.map(questions, (value, index) => {
+          switch (value.type) {
+            case 'text': {
+              objectsToReturn.push({text: `${index + 1}. ${value.name}`, style: 'header'});
+              objectsToReturn.push({
+                text: `Proszę udzielić odpowiedzi pisemnej.`,
+                style: 'small',
+                margin: [18, 2, 0, 150]
+              });
+              break;
+            }
+
+            case 'scale': {
+              objectsToReturn.push({
+                text: `${index + 1}. ${value.name} Odpowiedz w skali od ${value.scaleMin} do ${value.scaleMax}.`,
+                style: 'header',
+                margin: [0, 0, 0, 40]
+              });
+              break;
+            }
+
+            case 'number': {
+              objectsToReturn.push({text: `${index + 1}. ${value.name}`, style: 'header'});
+              objectsToReturn.push({text: `Odpowiedzią powinna być liczba.`, style: 'small', margin: [18, 2, 0, 150]});
+              break;
+            }
+
+            case 'select': {
+              objectsToReturn.push({text: `${index + 1}. ${value.name}`, style: 'header'});
+              objectsToReturn.push({
+                text: `Proszę zaznaczyć jedną z odpowiedzi.`,
+                style: 'small',
+                margin: [18, 2, 0, 0]
+              });
+
+              let possibleAnswers = {ul: [], margin: [15, 8, 0, 20]};
+              for (let i = 0; i < value.possibleAnswers.length; i++) {
+                possibleAnswers.ul.push(value.possibleAnswers[i]);
+              }
+              objectsToReturn.push(possibleAnswers);
+              break;
+            }
+          }
+
+          return objectsToReturn;
+        });
+
+
+        let docDefinition = {
+          content: [processedQuestions],
+          styles: {
+            header: {
+              bold: true,
+              fontSize: 15
+            },
+            small: {
+              fontSize: 7.5,
+              italics: true
+            }
           },
-          this.$data.margins);
+          defaultStyle: {
+            fontSize: 12
+          }
+        };
 
-        pdf.save('test.pdf');
-
-
-        // let pdf = new jsPDF('p', 'pt', 'letter'), source = this.$refs.page_content, specialElementHandlers = {
-        //   '#bypassme': function (element, renderer) {
-        //     return true
-        //   }
-        // };
-        // var margins = {
-        //   top: 60,
-        //   bottom: 60,
-        //   left: 40,
-        //   width: 522
-        // };
-        // pdf.fromHTML(source, margins.left, margins.top,
-        //   {
-        //     'width': margins.width
-        //     , 'elementHandlers': specialElementHandlers
-        //   },
-        //   function () {
-        //     pdf.save('download_pdf.pdf');
-        //   }, margins);
-
-
-        // const doc = new jsPDF();
-        // let height = 0;
-        // for (let i = 0; i < this.questions.length; i++) {
-        //   doc.setFontSize(12);
-        //   height += 10;
-        //
-        //   doc.text((i + 1).toString() + ". " + questions[i].name, 10, height + 10);
-        //
-        //   height += 6;
-        //   doc.setFontSize(10);
-        //
-        //   switch (questions[i].type) {
-        //     case 'text':
-        //       doc.setLineWidth(5);
-        //       doc.text('Pytanie otwarte', 10, height + 10);
-        //       break;
-        //     case 'scale':
-        //       doc.text('Pytanie skali (' + questions[i].scaleMin + ' - ' + questions[i].scaleMax + ')', 10, height + 10);
-        //       break;
-        //     case 'number':
-        //       doc.text('Pytanie liczbowe', 10, height + 10);
-        //       break;
-        //     case 'select':
-        //       let text = 'Pytanie wyboru: ';
-        //       for (let j = 0; j < this.questions[i].possibleAnswers.length; j++) {
-        //         if (j === 0) {
-        //           text += questions[i].possibleAnswers[j];
-        //         } else {
-        //           text += ", " + questions[i].possibleAnswers[j];
-        //         }
-        //       }
-        //       doc.text(text, 10, height + 10);
-        //       break;
-        //   }
-        //
-        //   height += 5;
-        // }
-        //
-        // doc.save('test.pdf');
+        pdfMake.createPdf(docDefinition).download();
       }
     },
   }
