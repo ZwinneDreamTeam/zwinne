ecfe4419355520c379be47cf89a7ab43a272c435
Izabela Zbieć <izbiec@stud.ics.p.lodz.pl>
2018-04-23 11:47:51 +0200

    Changed searching for marked results

--- src/components/tests/markTest/ResolvedTestsList.vue
+++ src/components/tests/markTest/ResolvedTestsList.vue
@@ -11,7 +11,7 @@
         <md-table-cell md-label="Stanowisko" md-sort-by="positionName">{{ item.positionName }}</md-table-cell>
         <md-table-cell md-label="Właściciel" md-sort-by="ownerName">{{ item.ownerName }}</md-table-cell>
         <md-table-cell md-label="Kandydat" md-sort-by="candidateUsername">{{ item.candidateUsername }}</md-table-cell>
-        <div v-if="currentUserAuthUid === item.ownerId && !item.answers[0].mark">
+        <div v-if="currentUserAuthUid === item.ownerId && !item.marked">
            <md-button @click.native="onSelect(item)" class="md-raised md-primary"> Oceń </md-button>
         </div>
       </md-table-row>
@@ -42,6 +42,8 @@
         .orderByChild('ownerUid')
         .on('child_added', (snapshot) => {
           let result = snapshot.val();
+          if(result.answers[0].mark === "") result.marked = false;
+          else result.marked = true;
           result.key = snapshot.key;
           let candidateUsernamePromise = db.ref('users/' + result.candidateId + "/username").once('value')
            .then(function(snapshot) {
