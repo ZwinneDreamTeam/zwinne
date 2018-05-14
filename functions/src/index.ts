7369e8b31dafb3b2b5dd75594a16a152b9675300
Patryk Kacperski <221599@edu.p.lodz.pl>
2018-05-14 12:51:04 +0200

    Updated functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -21,3 +21,29 @@ export const OnCreateFunction = functions.auth.user().onCreate((event) => {
     }
   });
 });
+
+const nodemailer = require('nodemailer');
+// const gmailEmail = functions.config().gmail.email;
+// const gmailPassword = functions.config().gmail.password;
+
+const APP_NAME = 'ZwinneXD';
+
+const mailTransport = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+    user: 'zwinne.kod.schrodingera@gmail.com',
+    pass: 'zwinne1234',
+  },
+});
+
+exports.sendEvaluation = functions.https.onRequest((email, message) => {
+  const mailOptions = {
+    from: `${APP_NAME} <noreply@firebase.com>`,
+    to: email,
+    subject: "Evaluation",
+    text: message
+  };
+  return mailTransport.sendMail(mailOptions).then(() => {
+    return console.log('Evaluation email send to :', email);
+  });
+});
